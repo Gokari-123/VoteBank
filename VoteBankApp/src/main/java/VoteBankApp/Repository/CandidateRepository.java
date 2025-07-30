84806dc8f5e3b5cf30828881ad73d0612891cfe7
Gokari-123 <mahaboobgokari@gmail.com>
2025-07-30 16:59:41 +0530

    Add files via upload

---
+++ VoteBankApp/src/main/java/VoteBankApp/Repository/CandidateRepository.java
@@ -0,0 +1,9 @@
+package VoteBankApp.Repository;
+
+import VoteBankApp.Entity.Candidate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CandidateRepository extends JpaRepository<Candidate,Long> {
+}
